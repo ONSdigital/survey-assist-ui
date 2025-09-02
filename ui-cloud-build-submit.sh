f8dbb066471619520f3e61ed92abcc12a7fd3222
Steve Gibbard <steve.gibbard@ons.gov.uk>
2025-09-02 17:23:27 +0100

    build(ui): update cloudbuild for new variables

--- ui-cloud-build-submit.sh
+++ ui-cloud-build-submit.sh
@@ -4,6 +4,10 @@
 
 set -e
 
+UI_VERSION=$(poetry version -s)
+GIT_SHA=$(git rev-parse --short=12 HEAD)
+BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
 echo "Files for upload: $(gcloud meta list-files-for-upload | tr '\n' ' ')"
 
 echo "Submitting Cloud Build for Survey Assist UI using:"
@@ -14,6 +18,8 @@ echo "  CICD_CLOUD_BUILD_SA: $CICD_CLOUD_BUILD_SA"
 echo "  CICD_CLOUD_BUILD_BUCKET: $CICD_CLOUD_BUILD_BUCKET"
 echo "  DEPLOY_PROJECT_ID: $DEPLOY_PROJECT_ID"
 echo "  UI_GAR: $UI_GAR"
+echo "  GIT_SHA: $GIT_SHA"
+echo "  BUILD_DATE: $BUILD_DATE"
 
 # check if user would like to proceed
 read -p "Do you want to proceed with the Cloud Build submission? (y/n) " -n 1 -r
@@ -27,5 +33,5 @@ gcloud builds submit --config cloudbuild.yaml \
     --project $CICD_PROJECT_ID \
     --region $REGION \
     --service-account $CICD_CLOUD_BUILD_SA \
-    --substitutions _REGION=$REGION,_ARTIFACT_REPO_NAME=$UI_GAR,_IMAGE_NAME=$UI_IMAGE_NAME,_IMAGE_TAG=$UI_VERSION,_DEPLOY_PROJECT_ID=$DEPLOY_PROJECT_ID \
+    --substitutions _REGION=$REGION,_ARTIFACT_REPO_NAME=$UI_GAR,_IMAGE_NAME=$UI_IMAGE_NAME,_IMAGE_TAG=$UI_VERSION,_DEPLOY_PROJECT_ID=$DEPLOY_PROJECT_ID,_BUILD_DATE=$BUILD_DATE,_GIT_SHA=$GIT_SHA\
     --gcs-source-staging-dir gs://$CICD_CLOUD_BUILD_BUCKET/$UI_IMAGE_NAME/$UI_VERSION
