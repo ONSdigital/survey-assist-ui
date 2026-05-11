236bf60e86f317fe9397857c2cc99de30e2e0da5
rupertons <rupert.anderson@ext.ons.gov.uk>
2026-05-11 16:50:55 +0100

    fix: mutiple fixes; pushing for testing

--- run_cicd_soc_to_sandbox.sh
+++ run_cicd_soc_to_sandbox.sh
@@ -18,9 +18,16 @@ CICD_SA=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(s
 CR_BUCKET=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['cr-bucket'])")
 REGION=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['region'])")
 
-OTP_URL=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['cr-otp-api-url'])")
-API_URL=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['cr-api-url'])")
-API_SA_EMAIL=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['api-sa-email'])")
+OTP_URL=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['cr-otp-api-url'])") # ok
+API_URL=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['cr-api-url'])") # ok
+
+API_SA_EMAIL=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['apigw-sa-email'])")
+ 
+# _BACKEND_SA_EMAIL should be backend-api-access@survey-assist-sandbox.iam.gserviceaccount.com
+
+# NEW Params required:
+PROXY_API_URL="https://proxy-api-670504361336.europe-west2.run.app"
+GAR_IMAGE="europe-west2-docker.pkg.dev/survey-assist-sandbox/survey-assist-ui/survey-assist-ui"
 
 CB_BUCKET=gs://${PROJECT_ID}_cloudbuild/survey-assist-ui
 
@@ -28,7 +35,7 @@ gcloud beta builds submit . --config=cicd/cloudbuild_dev_and_sandbox.yaml \
 	--project $CICD_PROJECT_ID \
 	--service-account projects/$CICD_PROJECT_ID/serviceAccounts/$CICD_SA \
 	--gcs-source-staging-dir $CB_BUCKET \
-	--substitutions=SHORT_SHA=$GIT_SHA,_BACKEND_API_URL=$API_URL,_BACKEND_SA_EMAIL=$API_SA_EMAIL,_GAR_IMAGE="europe-west2-docker.pkg.dev/survey-assist-sandbox/survey-assist-ui/survey-assist-ui",_GUNICORN_WORKERS=6,_TARGET_PROJECT_ID=$PROJECT_ID,_UI_SA_EMAIL="survey-ui-cloud-run@survey-assist-sandbox.iam.gserviceaccount.com",_VERIFY_API_URL=$OTP_URL \
+	--substitutions=SHORT_SHA=$GIT_SHA,_BACKEND_API_URL=$API_URL,_BACKEND_SA_EMAIL=$API_SA_EMAIL,_GAR_IMAGE=$GAR_IMAGE,_PROXY_API_URL=$PROXY_API_URL,_GUNICORN_WORKERS=6,_TARGET_PROJECT_ID=$PROJECT_ID,_VERIFY_API_URL=$OTP_URL \
 	--region $REGION
 
 #_BACKEND_API_URL=https://survey-assist-api-670504361336.europe-west2.run.app
