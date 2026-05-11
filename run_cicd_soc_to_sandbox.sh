eca317e2ec73b5a8791db0ddf4a2bea5b7d10f9f
rupertons <rupert.anderson@ext.ons.gov.uk>
2026-05-11 17:19:08 +0100

    fix: use param store to lookup config

--- run_cicd_soc_to_sandbox.sh
+++ run_cicd_soc_to_sandbox.sh
@@ -3,6 +3,7 @@
 # This script is only intended for use in the Sandbox environment.
 #
 # Please set the environment variable CICD_PROJECT_ID i.e. export CICD_PROJECT_ID=
+ENV_NAME=sandbox # Sandbox use only
 
 if [[ ! -v CICD_PROJECT_ID ]]; then 
    echo "Please set the environment variable CICD_PROJECT_ID i.e. export CICD_PROJECT_ID="
@@ -12,6 +13,7 @@ fi
 ENV_NAME=sandbox # Sandbox use only
 
 GIT_SHA=$(git rev-parse --short HEAD)
+API_VERSION="v1"
 sandbox_config=$(gcloud parametermanager parameters versions describe $ENV_NAME --parameter=infra-test-config --location=global --project $CICD_PROJECT_ID --format=json | python3 -c "import sys, json; print(json.load(sys.stdin)['payload']['data'])" | base64 --decode)
 PROJECT_ID=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['project-id'])")
 CICD_SA=$(echo $sandbox_config | python3 -c "import sys, json; print(json.load(sys.stdin)['cicd-sa-email'])")
@@ -35,7 +37,7 @@ gcloud beta builds submit . --config=cicd/cloudbuild_dev_and_sandbox.yaml \
 	--project $CICD_PROJECT_ID \
 	--service-account projects/$CICD_PROJECT_ID/serviceAccounts/$CICD_SA \
 	--gcs-source-staging-dir $CB_BUCKET \
-	--substitutions=SHORT_SHA=$GIT_SHA,_BACKEND_API_URL=$API_URL,_BACKEND_SA_EMAIL=$API_SA_EMAIL,_GAR_IMAGE=$GAR_IMAGE,_PROXY_API_URL=$PROXY_API_URL,_GUNICORN_WORKERS=6,_TARGET_PROJECT_ID=$PROJECT_ID,_VERIFY_API_URL=$OTP_URL \
+	--substitutions=_ENV_NAME=$ENV_NAME,SHORT_SHA=$GIT_SHA,_API_VERSION=$API_VERSION,_BACKEND_API_URL=$API_URL,_BACKEND_SA_EMAIL=$API_SA_EMAIL,_GAR_IMAGE=$GAR_IMAGE,_PROXY_API_URL=$PROXY_API_URL,_GUNICORN_WORKERS=6,_TARGET_PROJECT_ID=$PROJECT_ID,_VERIFY_API_URL=$OTP_URL \
 	--region $REGION
 
 #_BACKEND_API_URL=https://survey-assist-api-670504361336.europe-west2.run.app
